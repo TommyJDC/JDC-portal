c64138321651560a888038e30de3a120368b4842
InFerno <50930990+dioisconnect@users.noreply.github.com>
2025-04-26 15:11:44 +0200

    dashboard loading5

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,7 +4,6 @@ import { hydrateRoot } from "react-dom/client";
 
 // Import FontAwesome config and disable auto CSS addition
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
 config.autoAddCss = false;
 
 startTransition(() => {
